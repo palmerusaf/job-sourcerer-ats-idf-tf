a9359c8908c9ed507def4d4cb4c27169c69ad4a7
Branden Palmer <brandenpalmer08@gmail.com>
2026-04-20 00:03:40 -0500

    add output

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -20,12 +21,20 @@ df = df1["description"].apply(clean_html)
 df = pd.concat([df, df2], ignore_index=True)
 
 vectorizer = TfidfVectorizer(
-    token_pattern=r"[a-z0-9+#]+", lowercase=True, stop_words="english", min_df=2
+    token_pattern=r"(?u)\b[a-zA-Z][a-zA-Z0-9+#.\-]{1,}\b",
+    lowercase=True,
+    stop_words="english",
+    min_df=5,
 )
 X = vectorizer.fit_transform(df)
 terms = vectorizer.get_feature_names_out()
 idf = vectorizer.idf_
 
-im = {term: float(weight) for term, weight in zip(terms, idf)}
-# im = [[k, v] for k, v in im.items()]
-# im.sort(key=lambda i: i[1])
+im = {term: float(min(weight, 5)) for term, weight in zip(terms, idf)}
+with open("./idf.json", "w") as f:
+    json.dump(im, f)
+im = [[k, v] for k, v in im.items()]
+im.sort(key=lambda i: i[1])
+for k, v in im:
+    print(f"{k}\t\t\t\t{v}")
+print(len(im))
